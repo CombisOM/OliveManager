10e245f8cffbc6ed8e3fc819fc134e8903b84175
Mladen Maras <mladen.maras@gmail.com>
2016-05-06 15:54:38 +0200

    Add project files.

---
+++ OliveManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OliveManager.Startup))]
+namespace OliveManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
